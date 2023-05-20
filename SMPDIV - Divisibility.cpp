6023458a50baaa0c42dbbdfe0a68e76fca3d454b
niteesh1407 <117834365+Niteesh1407@users.noreply.github.com>
2023-05-20 15:01:39 +0530

    Create SMPDIV - Divisibility.cpp

---
+++ SMPDIV - Divisibility.cpp
@@ -0,0 +1,19 @@
+//link: https://www.spoj.com/problems/SMPDIV/
+
+CODE:
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+       int n,x,y;
+       cin>>n>>x>>y;
+       for(int i=2;i<n;i++){
+           if(i%x==0 && i%y!=0)   
+           cout<<i<<" ";
+       }
+    cout<<endl;
+    }
+}
